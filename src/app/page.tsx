780fe1bbc1cb9478ef742840d0680b18712a76d3
Abdelillahze <Abdelillahzegaoui@gmail.com>
2023-10-21 22:09:37 +0100

    finish

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,113 +1,174 @@
-import Image from 'next/image'
+"use client";
+import axios from "axios";
+import Image from "next/image";
+import { useState, useRef, useEffect } from "react";
+import { BiBookAlt, BiSearch } from "react-icons/bi";
+import { FaPause } from "react-icons/fa";
+import { BsFillPlayFill } from "react-icons/bs";
+import { GiOpenBook } from "react-icons/gi";
+import { BsFillMoonFill } from "react-icons/bs";
+import { PiSunDimLight } from "react-icons/pi";
 
 export default function Home() {
+  const audioRef = useRef<HTMLAudioElement>(null);
+  const [mode, setMode] = useState("");
+  const [word, setWord] = useState("");
+  const [data, setData] = useState<any>(null);
+  const [start, setStart] = useState(false);
+
+  useEffect(() => {
+    if (!localStorage.getItem("mode")) {
+      localStorage.setItem("mode", "light");
+      return setMode("light");
+    }
+    setMode(localStorage.getItem("mode")!);
+  }, []);
+
+  useEffect(() => {
+    if (!audioRef.current) return;
+
+    audioRef.current.addEventListener("ended", () => {
+      setStart(false);
+    });
+
+    if (start) {
+      audioRef.current.play();
+    } else {
+      audioRef.current.pause();
+    }
+  }, [start]);
+
+  const search = async () => {
+    if (!word.trim()) return;
+    try {
+      const res = await axios.get(
+        `https://api.dictionaryapi.dev/api/v2/entries/en/${word}`
+      );
+      const response = await res.data;
+
+      if (response.status === 404) {
+        setData(false);
+      } else {
+        setData(response[0]);
+      }
+    } catch (e) {
+      console.log(e);
+      setData(false);
+    }
+  };
+
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <div className="z-10 max-w-5xl w-full items-center justify-between font-mono text-sm lg:flex">
-        <p className="fixed left-0 top-0 flex w-full justify-center border-b border-gray-300 bg-gradient-to-b from-zinc-200 pb-6 pt-8 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:static lg:w-auto  lg:rounded-xl lg:border lg:bg-gray-200 lg:p-4 lg:dark:bg-zinc-800/30">
-          Get started by editing&nbsp;
-          <code className="font-mono font-bold">src/app/page.tsx</code>
-        </p>
-        <div className="fixed bottom-0 left-0 flex h-48 w-full items-end justify-center bg-gradient-to-t from-white via-white dark:from-black dark:via-black lg:static lg:h-auto lg:w-auto lg:bg-none">
-          <a
-            className="pointer-events-none flex place-items-center gap-2 p-8 lg:pointer-events-auto lg:p-0"
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            By{' '}
-            <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className="dark:invert"
-              width={100}
-              height={24}
-              priority
+    <div
+      className={`${
+        mode === "light" ? "bg-gray-50 text-black" : "bg-gray-900 text-white"
+      } w-full min-h-screen h-full mx-auto`}
+    >
+      <div className="w-full px-6 lg:w-3/5 mx-auto">
+        <header className="flex h-24 justify-between items-center py-4">
+          <BiBookAlt className="text-4xl text-gray-400" />
+          {mode === "light" ? (
+            <BsFillMoonFill
+              onClick={() => {
+                localStorage.setItem("mode", "dark");
+                setMode("dark");
+              }}
+              className="text-gray-400 text-xl cursor-pointer"
             />
-          </a>
-        </div>
+          ) : (
+            <PiSunDimLight
+              onClick={() => {
+                localStorage.setItem("mode", "light");
+                setMode("light");
+              }}
+              className={`text-gray-400 text-2xl cursor-pointer`}
+            />
+          )}
+        </header>
+        <section className="w-full">
+          <div className="relative mb-6">
+            <input
+              onKeyDown={(e) => {
+                if (e.key === "Enter") {
+                  search();
+                }
+              }}
+              value={word}
+              onChange={(e) => {
+                setWord(e.target.value);
+                setData(null);
+              }}
+              type="text"
+              placeholder="type any word"
+              className="w-full bg-gray-300 outline-none py-4 px-6 rounded placeholder:font-normal placeholder:text-gray-600 text-black font-semibold"
+            />
+            <BiSearch
+              onClick={search}
+              className="cursor-pointer absolute right-4 top-1/2 -translate-y-1/2 text-xl text-main-100"
+            />
+          </div>
+          {/* info */}
+          {data === false ? (
+            <div className="text-2xl text-center mt-20">
+              There is no result to <span className="font-bold">{word}</span>
+            </div>
+          ) : data ? (
+            <div className="relative flex flex-col">
+              <div className="flex justify-between items-center">
+                <h1 className="text-4xl font-bold capitalize mb-2">
+                  {data.word}
+                </h1>
+                <audio
+                  ref={audioRef}
+                  src={
+                    data.phonetics.find((e: any) => e.audio.length > 0).audio
+                  }
+                ></audio>
+                <div
+                  onClick={() => setStart(!start)}
+                  className="w-14 h-14 cursor-pointer rounded-full bg-main-50 flex items-center justify-center"
+                >
+                  {start ? (
+                    <FaPause className="text-main-100 text-xl" />
+                  ) : (
+                    <BsFillPlayFill className="text-main-100 text-3xl" />
+                  )}
+                </div>
+              </div>
+              <span className="text-main-100 mb-6">{data.phonetic}</span>
+              <span className="font-semibold text-xl mb-4">noun</span>
+              <span className="text-xl text-gray-600 font-light mb-3">
+                Meaning
+              </span>
+              <ul className="pl-12 mb-6">
+                {data.meanings[0].definitions.map((e: any) => {
+                  return (
+                    <li className="relative mb-2 before:content-['•'] before:text-main-100 before:absolute before:-left-6 before:-translate-y-[2px] before:text-xl">
+                      {e.definition}
+                    </li>
+                  );
+                })}
+              </ul>
+              <div className="flex items-center mb-8 flex-wrap">
+                <span className="text-xl font-light text-gray-600 mr-2">
+                  Synonyms
+                </span>
+                {data.meanings[0].synonyms.map((e: string) => {
+                  return (
+                    <span className="text-main-100 font-bold text-lg mr-2">
+                      {e}
+                    </span>
+                  );
+                })}
+              </div>
+            </div>
+          ) : (
+            <div className="w-full h-full flex flex-col items-center justify-center">
+              <GiOpenBook className="text-[250px] text-gray-400" />
+              <p className="text-gray-600">Search of a word </p>
+            </div>
+          )}
+        </section>
       </div>
-
-      <div className="relative flex place-items-center before:absolute before:h-[300px] before:w-[480px] before:-translate-x-1/2 before:rounded-full before:bg-gradient-radial before:from-white before:to-transparent before:blur-2xl before:content-[''] after:absolute after:-z-20 after:h-[180px] after:w-[240px] after:translate-x-1/3 after:bg-gradient-conic after:from-sky-200 after:via-blue-200 after:blur-2xl after:content-[''] before:dark:bg-gradient-to-br before:dark:from-transparent before:dark:to-blue-700 before:dark:opacity-10 after:dark:from-sky-900 after:dark:via-[#0141ff] after:dark:opacity-40 before:lg:h-[360px] z-[-1]">
-        <Image
-          className="relative dark:drop-shadow-[0_0_0.3rem_#ffffff70] dark:invert"
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
-        />
-      </div>
-
-      <div className="mb-32 grid text-center lg:max-w-5xl lg:w-full lg:mb-0 lg:grid-cols-4 lg:text-left">
-        <a
-          href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Docs{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Find in-depth information about Next.js features and API.
-          </p>
-        </a>
-
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Learn{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Learn about Next.js in an interactive course with&nbsp;quizzes!
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Templates{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Explore the Next.js 13 playground.
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Deploy{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
-          </p>
-        </a>
-      </div>
-    </main>
-  )
+    </div>
+  );
 }
--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,20 +1,37 @@
-import type { Config } from 'tailwindcss'
+import type { Config } from "tailwindcss";
 
 const config: Config = {
   content: [
-    './src/pages/**/*.{js,ts,jsx,tsx,mdx}',
-    './src/components/**/*.{js,ts,jsx,tsx,mdx}',
-    './src/app/**/*.{js,ts,jsx,tsx,mdx}',
+    "./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
+    "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
+    "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
   ],
   theme: {
     extend: {
       backgroundImage: {
-        'gradient-radial': 'radial-gradient(var(--tw-gradient-stops))',
-        'gradient-conic':
-          'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))',
+        "gradient-radial": "radial-gradient(var(--tw-gradient-stops))",
+        "gradient-conic":
+          "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
+      },
+      colors: {
+        main: {
+          100: "#a442ef",
+          50: "#e8d0fd",
+        },
+        gray: {
+          100: "#F8F9FA",
+          200: "#f4f4f4",
+          300: "#DEE2E6",
+          400: "#CED4DA",
+          500: "#ADB5BD",
+          600: "#6C757D",
+          700: "#495057",
+          800: "#343A40",
+          900: "#212529",
+        },
       },
     },
   },
   plugins: [],
-}
-export default config
+};
+export default config;
